7074dfc76962379d7a4e083af9058c972d90438f
YuyaMatsushima <matsushimayuya@matsushima-no-MacBook-Air.local>
2014-06-08 14:54:41 +0900

    データベースに送信

--- Nanpa/ViewController.m
+++ Nanpa/ViewController.m
@@ -13,11 +13,21 @@
 @end
 
 @implementation ViewController
-//松島
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSString *url = @"http://54.178.229.23/users.php";
+    NSMutableURLRequest *request = [NSMutableURLRequest new];
+    [request setURL:[NSURL URLWithString:url]];
+    NSString *body =[NSString stringWithFormat:@"name=ともや&image=こんにちわ"];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",returnString);
+
 }
 
 - (void)didReceiveMemoryWarning
